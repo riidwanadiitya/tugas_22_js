6a34736224a2b5f2f5ab573785e8350c17c1f800
Ridwan <71011389+riidwanadiitya@users.noreply.github.com>
2021-01-04 09:39:01 +0700

    TUGAS 22 JS

---
+++ tugas22.js
@@ -0,0 +1,9 @@
+function tugas22Js() {
+    var data = "Saya ingin belajar besama";
+    var dataArray = data.split(" ");
+    dataArray.forEach(function(item, index) {
+        console.log("item : " + item + " indeks ke " + index);
+
+    });
+}
+tugas22Js()
